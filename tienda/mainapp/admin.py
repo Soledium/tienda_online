c088220b2c101a0a7a6935b3c7f67be7380d1a21
Soledium <nescobar3@hotmail.com>
2025-11-17 11:52:34 -0300

    actualizacion 1
    settings, models y admin.py

--- tienda/mainapp/admin.py
+++ tienda/mainapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Producto, Categoria, Insumo
 
 # Register your models here.
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'categoria', 'precio')
+    search_fields = ('nombre', 'descripcion')
+    list_filter = ('categoria',)
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin): 
+    list_display = ('nombre',)
+    search_fields = ('nombre', 'descripcion')
+
+
+@admin.register(Insumo)
+class InsumoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'cantidad', 'proveedor')
+    search_fields = ('nombre', 'proveedor') 
--- tienda/mainapp/models.py
+++ tienda/mainapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+    
+
+class Insumo(models.Model):
+    nombre = models.CharField(max_length=100)
+    cantidad = models.IntegerField()
+    proveedor = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
