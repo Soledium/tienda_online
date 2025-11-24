a81ef0d6de0d7b9792bb7d007d0adb6bef5e3e29
Soledium <nescobar3@hotmail.com>
2025-11-24 11:41:42 -0300

    modi

--- tienda/mainapp/models.py
+++ tienda/mainapp/models.py
@@ -12,13 +12,14 @@ class Producto(models.Model):
     def __str__(self):
         return self.nombre
 
+
 class Categoria(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField()
 
     def __str__(self):
         return self.nombre
-    
+
 
 class Insumo(models.Model):
     nombre = models.CharField(max_length=100)
@@ -27,3 +28,12 @@ class Insumo(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+class Pedido(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.IntegerField()
+    fecha_pedido = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Pedido de {self.cantidad} x {self.producto.nombre} el {self.fecha_pedido}"
