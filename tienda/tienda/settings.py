4bca4ff47b3a304d37da03c1ab252d93acd4545f
Soledium <nescobar3@hotmail.com>
2025-12-14 22:46:35 -0300

    .

--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -43,8 +43,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'mainapp'
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
